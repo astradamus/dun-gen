7feb655befe569cdb89f68fea8e298643d05e72b
Alexander Strada <astrada2023@gmail.com>
2016-12-08 16:04:33 -0600

    Add relative turning methods to enum Direction
     - Methods left(), right(), leftCardinal(), and rightCardinal() return
        the next Direction object to the left (counter-clockwise) or to the
        right (clockwise) around the compass.
     - The cardinal methods skip diagonal directions.

--- src/com/alexanderstrada/dun_gen/map/Direction.java
+++ src/com/alexanderstrada/dun_gen/map/Direction.java
@@ -4,22 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Direction {
-    NORTH(0, -1),
-    EAST(1, 0),
-    SOUTH(0, 1),
-    WEST(-1, 0),
+    NORTH(0, +0, -1),
+    NE   (1, +1, -1),
+    EAST (2, +1, +0),
+    SE   (3, +1, +1),
+    SOUTH(4, +0, +1),
+    SW   (5, -1, +1),
+    WEST (6, -1, +0),
+    NW   (7, -1, -1);
 
-    NW(-1, -1),
-    NE(1, -1),
-    SW(-1, 1),
-    SE(1, 1);
-
+    public final int id;
     public final int offX;
     public final int offY;
 
-    Direction(int offX, int offY) {
+    Direction(int id, int offX, int offY) {
+        this.id = id;
         this.offX = offX;
         this.offY = offY;
+    }
+
+    public Direction left() {
+        return values()[calcLeft()];
+    }
+
+    public Direction right() {
+        return values()[(id+1)%values().length];
+    }
+
+    public Direction leftCardinal() {
+        int i = calcLeft();
+        i -= (i%2);
+        return values()[i];
+    }
+
+    public Direction rightCardinal() {
+        int i = (id+1);
+        i += (i%2);
+        i %= values().length;
+        return values()[i];
+    }
+
+    private int calcLeft() {
+        return (id - 1 + values().length) % values().length;
     }
 
     public static List<Direction> getCardinals() {
