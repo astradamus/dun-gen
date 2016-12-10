c9c70a82d7dc562a8563e707e56b0b168ff3e6fc
Alexander Strada <astrada2023@gmail.com>
2016-12-09 19:06:00 -0600

    Use integers instead of Vectors in SealDeadEnds

--- src/com/alexanderstrada/dun_gen/gen/process/SealDeadEnds.java
+++ src/com/alexanderstrada/dun_gen/gen/process/SealDeadEnds.java
@@ -4,7 +4,6 @@ import com.alexanderstrada.dun_gen.Utils;
 import com.alexanderstrada.dun_gen.gen.BasicGenerator;
 import com.alexanderstrada.dun_gen.map.Direction;
 import com.alexanderstrada.dun_gen.map.Map;
-import com.alexanderstrada.dun_gen.map.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +11,9 @@ import java.util.Random;
 
 public class SealDeadEnds extends BasicGenerator {
 
-    private final List<Vector> deadEnds = new ArrayList<>();
-    private final List<Vector> deadEndCandidates = new ArrayList<>();
-    private final List<Vector> working = new ArrayList<>();
+    private final List<Integer> deadEnds = new ArrayList<>();
+    private final List<Integer> deadEndCandidates = new ArrayList<>();
+    private final List<Integer> working = new ArrayList<>();
 
     public SealDeadEnds(Random random) {
         super(random);
@@ -37,8 +36,8 @@ public class SealDeadEnds extends BasicGenerator {
             working.addAll(deadEndCandidates);
             deadEndCandidates.clear();
 
-            for (Vector v : working) {
-                scanVector(v);
+            for (int i : working) {
+                scanIndex(i);
             }
 
             working.clear();
@@ -47,24 +46,24 @@ public class SealDeadEnds extends BasicGenerator {
 
     private void findDeadEnds() {
         for (int i = 0; i < tiles.length; i++) {
-            scanVector(Utils.getVectorFromIndex(i, height));
+            scanIndex(i);
         }
     }
 
     private void sealDeadEnds(long updateDelay) {
-        for (Vector v : deadEnds) {
-            tiles[v.toArrayIndex(height)] = Map.WALL_TILE;
+        for (int i : deadEnds) {
+            tiles[i] = Map.WALL_TILE;
             notifyGenerationListener();
             Utils.maybeWait(this, updateDelay);
         }
         deadEnds.clear();
     }
 
-    private void scanVector(Vector v) {
-        if (tiles[v.toArrayIndex(height)] != Map.WALL_TILE) {
-            List<Vector> openNeighbors = map.getOpenNeighbors(v, Direction.getCardinals());
+    private void scanIndex(int i) {
+        if (tiles[i] != Map.WALL_TILE) {
+            List<Integer> openNeighbors = map.getOpenNeighbors(i, Direction.getCardinals());
             if (openNeighbors.size() <= 1) {
-                deadEnds.add(v);
+                deadEnds.add(i);
                 deadEndCandidates.addAll(openNeighbors);
             }
         }
