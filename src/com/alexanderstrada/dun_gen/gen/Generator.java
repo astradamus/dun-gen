6e57fa4335e40362feb9ad470f6266a64117a1b2
Alexander Strada <astrada2023@gmail.com>
2016-12-16 12:35:09 -0600

    Remove unnecessary full package declaration in Generator

--- src/com/alexanderstrada/dun_gen/gen/Generator.java
+++ src/com/alexanderstrada/dun_gen/gen/Generator.java
@@ -3,6 +3,6 @@ package com.alexanderstrada.dun_gen.gen;
 import com.alexanderstrada.dun_gen.tile_map.TileMap;
 
 public interface Generator {
-    void setGenerationListener(com.alexanderstrada.dun_gen.gen.GenerationListener listener);
+    void setGenerationListener(GenerationListener listener);
     void apply(TileMap tileMap);
 }
